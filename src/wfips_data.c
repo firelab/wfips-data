8e9dc051fba31738106f233fc6b4fb04cd68b44a
Kyle Shannon <kyle@pobox.com>
2014-08-20 14:00:25 -0600

    init

--- src/wfips_data.c
+++ src/wfips_data.c
@@ -39,3 +39,14 @@
 ** For more information, please refer to <http://unlicense.org>
 **
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sqlite3.h"
+
+int main( int argc, char *argv[] )
+{
+    return 0;
+}
+
